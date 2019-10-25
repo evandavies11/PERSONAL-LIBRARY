62c538777d1a857b7b87340255e9faafd86f4203
evandavies11 <evandavies11@gmail.com>
2019-10-25 18:16:50 -0500

    have working links

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,11 +1,14 @@
 import React from "react";
+import './Nav.css';
 
 function Nav() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+    <nav className="navbar">
       <a className="navbar-brand" href="/">
         Your Own Personal Library
       </a>
+      <li className="nav-link"><a href="/Books">Your Books</a></li>
+      <li className="nav-link"><a href="/Search">Search Books</a></li>
     </nav>
   );
 }
