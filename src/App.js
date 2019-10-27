786acebac1c81b7011ec7a6d22fe47df294437a6
evandavies11 <evandavies11@gmail.com>
2019-10-26 20:12:32 -0500

    most of the components done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Books from "./pages/Books";
-import Detail from "./pages/Detail";
+//import Detail from "./pages/Detail";
 import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
+import Search from './pages/Search'
+import moduleName from './pages/Saved'
 
 function App() {
   return (
@@ -11,12 +13,13 @@ function App() {
       <div>
         <Nav />
 
-        <Switch>
-          <Route exact path="/" component={Books} />
-          <Route exact path="/books" component={Books} />
-          <Route exact path="/books/:id" component={Detail} />
-          <Route component={NoMatch} />
-        </Switch>
+
+        <Route exact path="/" component={Books} />
+        <Route exact path="/books" component={Books} />
+        <Route exact path="/Search" component={Search} />
+        <Route exact path="/Saved" component={Saved} />
+        <Route component={NoMatch} />
+
 
       </div>
     </Router >
---
+++ src/components/BookSearch/BookSearch.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import SearchArea from './SearchArea';
+import request from 'superagent';
+import BookList from './BookList'
+
+
+
+class Books extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            books: [],
+            searchField: ''
+        }
+    }
+
+    searchBook = (event) => {
+        event.preventDefault();
+        request
+            .get('https://www.googleapis.com/books/v1/volumes')
+            .query({ q: this.state.searchField })
+            .then((data) => {
+                console.log(data);
+
+                this.setState({
+                    books: [...data.body.items]
+                })
+            })
+    }
+
+    handleSearch = (event) => {
+        this.setState({ searchField: event.target.value })
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchArea searchBook={this.searchBook} handleSearch={this.handleSearch} />
+                <BookList books={this.state.books} />
+            </div >
+        );
+    }
+}
+
+
+export default Books;
